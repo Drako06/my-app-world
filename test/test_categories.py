7730f141d73fd774e7d55cabdcbf6ab78f410b65
Dario Santiago <dario_acs12@hotmail.com>
2024-07-10 03:00:26 -0600

    add docker files

--- test/test_categories.py
+++ test/test_categories.py
@@ -8,7 +8,7 @@ class TestCategories(unittest.TestCase):
     @atomic
     def test_create_category(self, session):
         response = client.post("/api/v1/categories/", json={"name": "Test Category"})
-        self.assertEqual(response.status_code, 200)  # Cambié a 201 ya que se espera al crear un nuevo recurso
+        self.assertEqual(response.status_code, 200)
         data = response.json()
         self.assertEqual(data["name"], "Test Category")
 
